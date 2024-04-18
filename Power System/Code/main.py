653527d402616bc83cc7eb84b0af8232197e1c0b
CameronParrettUKY <161110826+CameronParrettUKY@users.noreply.github.com>
2024-04-18 03:55:05 -0400

    Update main.py

--- Power System/Code/main.py
+++ Power System/Code/main.py
@@ -2,7 +2,7 @@ from machine import Pin, ADC,I2C,UART
 from time import sleep
 from picoslave import *
 Vdivide = ADC(Pin(28))
-Ishunt = ADC(Pin(27))
+Ishunt = ADC(Pin(27)) #Define Pins & setup realay 1 to enable 5v
 LED = Pin("LED", Pin.OUT)
 R1 = Pin(15,Pin.OUT)
 R2 = Pin(14,Pin.OUT)
@@ -15,28 +15,30 @@ Sig2 = Pin(5,Pin.OUT)
 #uart = UART(1, baudrate=9600, tx=Pin(4),rx=Pin(5))
 #uart.init(bits=8, parity=None,stop=1)
 while True:
-    Ishunt_Value = Ishunt.read_u16()/21845
-    Vdivide_value = Vdivide.read_u16()/21845
-    print("battery Volts Out:")
+    Ishunt_Value = Ishunt.read_u16()/21845 #Grab current reading
+    Vdivide_value = Vdivide.read_u16()/21845  #Grab voltage reading
+    print("battery Volts Out:") #print to console for debug
     print(Vdivide_value*40)
-    Vval=Vdivide_value*40
+    Vval=Vdivide_value*40 #Calculate real voltage
     print("Charge Current in:")
     print(Ishunt_Value/(1.1/6))
-    Ishunt_Value=Ishunt_Value/(1.1/6)
-    if Ishunt_Value > 1:
+    Ishunt_Value=Ishunt_Value/(1.1/6) #calculate real current
+    if Ishunt_Value > 1: # Are we charging?
         R1.off()
-        R2.off()
+        R2.off()  #Enter Standby Mode
         R3.off()
         R4.off()
-        Sig1.on()
+        Sig1.on() # Tell control sys we are charging
+        LED.on() #Indicate Charging
         elif:
             R1.on()
             R2.on()
-            R3.on()
+            R3.on() # Exit Standby Mode
             R4.on()
+            LED.off() 
             Sig1.off()
     if Vdivide_value <15
-        Sig2.on()
+        Sig2.on() # Tell control sys battery is low
         elif:
-            Sig2.off()
+            Sig2.off() # Battery no longer low
             
